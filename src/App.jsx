08ac75934f87c8f190abd15066399bc5cd820fa7
Martin <martin.peubey@gmail.com>
2024-01-03 16:33:30 +0100

    F02: Create routes and OrderPage, ErrorPage components

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,17 @@
+import { Routes, Route } from 'react-router-dom'
 import LoginPage from "./components/pages/login/LoginPage"
+import OrderPage from "./components/pages/order/OrderPage"
+import ErrorPage from "./components/pages/error/ErrorPage"
 
 function App() {
   // affichage
   return (
-    <div>
-      <LoginPage />
+    <div className="App">
+      <Routes>
+        <Route path='/' element={<LoginPage />}></Route>
+        <Route path='/order' element={<OrderPage/>}></Route>
+        <Route path='*' element={<ErrorPage/>}></Route>
+      </Routes>
     </div>
   )
 }
